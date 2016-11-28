d2fc575d08b3ac4ef5e92245fcdfef288479b96b
Jenny Söderberg <jenny.soderberg@ehalsomyndigheten.se>
2016-11-28 10:49:33 +0100

    Changes

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 Uppgift:
 Skriv en metod som skriver ut talen från 1 till 100.
-För multiplar av tre, skriv "Fizz" istället för talet och för multiplar av fem skriv "Buzz". För tal som är multiplar av BÅDE
-tre och fem, skriv "FizzBuzz". 
+Men för multiplar av tre, skriv "Fizz" istället för talet och för multiplar av fem skriv "Buzz".
+För tal som är multiplar av BÅDE tre och fem, skriv "FizzBuzz".
 
 Hur: 
 TDD ska användas och uppgiften ska lösas med hjälp av funktionallitet i Java8.
--- src/main/java/se/ehm/fizzbuzz/FizzBuzz.java
+++ src/main/java/se/ehm/fizzbuzz/FizzBuzz.java
@@ -1,13 +1,18 @@
 package se.ehm.fizzbuzz;
 
 /**
- * Hello world!
+ * FizzBuzz
+ *
+ * Skriv en metod som skriver ut talen från 1 till 100.
+ *
+ * Men för multiplar av tre, skriv "Fizz" istället för talet och för multiplar av fem skriv "Buzz".
+ * För tal som är multiplar av BÅDE tre och fem, skriv "FizzBuzz".
  *
  */
 public class FizzBuzz
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "FizzBuzz Exercise!" );
     }
 }
